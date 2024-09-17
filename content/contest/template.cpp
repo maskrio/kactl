dc0589d87af5f3c836662098f04c56c78124924c
maskrio <68728718+maskrio@users.noreply.github.com>
2024-09-17 19:46:03 +0700

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -7,10 +7,24 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+
 #define F first 
 #define S second
+#define fi first
+#define se second
+
 #define pb push_back
 #define mp make_pair
+#define ll long long
+#define llu unsigned long long
+#define pll pair<ll, ll>
+#define pllu pair<llu, llu>
+
+#define ld double
+#define pld pair<ld, ld>
+const ll INF = 9e18;
+const ll mods = 1e9+7;
+
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
